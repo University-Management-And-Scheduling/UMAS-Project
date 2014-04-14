19d43ef4fb69b66cf494c13137d05565f7b6d305
Simant <simant.purohit@gmail.com>
2014-04-14 01:17:47 -0500

    Added function signatures for main scheduling algorithm Adding code shortly

--- CS597-PROJECT/src/CourseSchedule.java
+++ CS597-PROJECT/src/CourseSchedule.java
@@ -58,8 +58,28 @@ public class CourseSchedule {
 		
 	}
 	
+	
 	public static void addCourseSchedule(CourseOffered courseoffered, Classroom classroom, Timeslots timeslots){
-		
+		//Check if the course offering is already scheduled
+		//Check if the course offering can be scheduled in the classroom mentioned including timeslot type etc
+		//If none of the above steps throw an error schedule the course
+	}
+	
+	public static void scheduleCourse(CourseOffered courseOffered){
+		//Check if the course is already scheduled
+		//Find a classroom with empty slot
+		//Schedule the course in the empty slot
+		//Commit the schedule
+	}
+	
+
+	public static void scheduleAllCurrentCourses(){
+		//Remove all the scheduled courses
+		//Get all the current course offerings
+		//Pick up one courseOffering at random
+		//Find a classroom and empty timeslot for the offering
+		//Schedule the courseOffering
+		//Repeat the steps with other offerings
 	}
 	
 	public void updateCourseSchedule(Classroom classroom, Timeslots timeslots){
@@ -70,9 +90,7 @@ public class CourseSchedule {
 		
 	}
 	
-	public static void scheduleAllCurrentCourses(){
-		
-	}
+	
 	
 	public static boolean hasConflict(){
 		return false;
