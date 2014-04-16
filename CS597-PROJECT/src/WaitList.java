fa6041cc69bd60eb4a96163e8a8d356e564eb980
Simant <simant.purohit@gmail.com>
2014-04-16 03:46:41 -0500

    Added various functions to help in generating wait list
    Added function signatures for waitlist

--- CS597-PROJECT/src/WaitList.java
+++ CS597-PROJECT/src/WaitList.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class WaitList {
 	int offerID;
@@ -43,28 +48,209 @@ public class WaitList {
 	public static void addStudentToWaitList(Student student, CourseOffered courseOffered){
 		
 	}
-	
-	public static void emailNextStudentForCourse(CourseOffered courseOffered){
-		
-	}
-	
-	public static void closeWaitList(CourseOffered courseOffered){
-		
-	}
-	
+			
 	public static void removeFromWaitList(Student student, CourseOffered courseOffered){
 		
 	}
 	
-	private static boolean isStudentFirstOnWaitList(Student student, CourseOffered courseOffered){
-		return false;
+	public static void emailFirstStudentOnWaitList(int offerID){
+		Student student = getStudentFirstOnWaitList(offerID);
+		
+		if(student!=null){
+			//add the student to emailed list
+			//remove the student from wait list
+			//email the student using the username
+		}
 	}
 	
-	private static boolean isStudentRegistered(Student student, CourseOffered courseOffered){
-		return false;
+	private static Student getStudentFirstOnWaitList(int offerID){
+		Student student = null;
+		if(!isWaitListEmpty(offerID)){
+			try{
+				Connection conn = Database.getConnection();
+				
+				try{
+					if(conn != null){
+						String SemesterSelect = "Select UIN"
+								+ " FROM university.waitlist"
+								+ " WHERE offerID= ? and QueuePos= ?";
+						PreparedStatement statement = conn.prepareStatement(SemesterSelect);
+						statement.setInt(1, offerID);
+						statement.setInt(2, 1);
+						ResultSet rs = statement.executeQuery();
+						if(rs.first()){
+							//return the student
+							student = new Student(rs.getInt("UIN"));
+						}
+																	
+					}
+				}
+				
+				catch(SQLException e){
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}			
+			}
+			
+			finally{
+				
+			}
+			
+		}
+		
+		return student;
 	}
 	
-	private static void checkTheStatusOfEmailedStudent(){
+	public static boolean isStudentRegistered(Student student, int offerID){
+		boolean isRegistered = false;
+		try{
+			Connection conn = Database.getConnection();
+			
+			try{
+				if(conn != null){
+					String SemesterSelect = "Select *"
+							+ " FROM university.studentenrollment"
+							+ " WHERE offerID= ? and UIN= ?";
+					PreparedStatement statement = conn.prepareStatement(SemesterSelect);
+					statement.setInt(1, offerID);
+					statement.setInt(2, student.getUIN());
+					ResultSet rs = statement.executeQuery();
+					if(rs.first()){
+						isRegistered = true;
+					}
+					
+					else{
+						isRegistered = false;
+					}
+										
+				}
+			}
+			
+			catch(SQLException e){
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}			
+		}
+		
+		finally{
+		}
+		
+		return isRegistered;
+	}
+	
+	public static boolean isStudentOnWaitList(Student student, int offerID){
+		boolean isOnWaitList = false;
+		try{
+			Connection conn = Database.getConnection();
+			
+			try{
+				if(conn != null){
+					String SemesterSelect = "Select *"
+							+ " FROM university.waitlist"
+							+ " WHERE offerID= ? and UIN= ?";
+					PreparedStatement statement = conn.prepareStatement(SemesterSelect);
+					statement.setInt(1, offerID);
+					statement.setInt(2, student.getUIN());
+					ResultSet rs = statement.executeQuery();
+					if(rs.first()){
+						isOnWaitList = true;
+					}
+					
+					else{
+						isOnWaitList = false;
+					}
+										
+				}
+			}
+			
+			catch(SQLException e){
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}			
+		}
+		
+		finally{
+		}
+		
+		return isOnWaitList;
+	}
+
+	public static boolean isWaitListEmpty(int offerID){
+		boolean isEmpty = false;
+		try{
+			Connection conn = Database.getConnection();
+			
+			try{
+				if(conn != null){
+					String SemesterSelect = "Select *"
+							+ " FROM university.waitlist"
+							+ " WHERE offerID= ?";
+					PreparedStatement statement = conn.prepareStatement(SemesterSelect);
+					statement.setInt(1, offerID);
+					ResultSet rs = statement.executeQuery();
+					if(rs.first()){
+						isEmpty = false;
+					}
+					
+					else{
+						isEmpty = true;
+					}
+										
+				}
+			}
+			
+			catch(SQLException e){
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}			
+		}
+		
+		finally{
+		}
+		
+		return isEmpty;
+	}
+	
+	//incomplete
+	public static boolean isStudentEmailed(Student student, int offerID){
+		boolean isEmailed = false;
+		try{
+			Connection conn = Database.getConnection();
+			
+			try{
+				if(conn != null){
+					String SemesterSelect = "Select *"
+							+ " FROM university.emailedwaitlist"
+							+ " WHERE offerID= ? and UIN= ?";
+					PreparedStatement statement = conn.prepareStatement(SemesterSelect);
+					statement.setInt(1, offerID);
+					statement.setInt(2, student.getUIN());
+					ResultSet rs = statement.executeQuery();
+					
+					if(rs.first()){
+						isEmailed = true;
+					}
+					
+					else{
+						isEmailed = false;
+					}
+										
+				}
+			}
+			
+			catch(SQLException e){
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		
+		finally{
+		}
+		
+		return isEmailed;
+	}
+	
+	private static void checkTheStatusOfEmailedStudents(){
 		
 	}
 	
