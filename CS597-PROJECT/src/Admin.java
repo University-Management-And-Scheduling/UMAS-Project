9085c14759266413cc935cde7a3a450d56add42f
Akshay <athirk2@uic.edu>
2014-04-17 20:52:18 -0500

    Employee- added and changed

--- CS597-PROJECT/src/Admin.java
+++ CS597-PROJECT/src/Admin.java
@@ -1,4 +1,40 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class Admin {
+
+
+public class Admin extends Employee {
+
+	public Admin(int UIN) {
+		super(UIN);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static boolean addAdmin(String name, Department dept) throws loginDetailsnotAdded{
+		
+		boolean isAdded=false;
+		
+		if(dept==null){
+			
+			return false;
+		}
+		
+		int addedUIN=Employee.addIntoDatabase(name, dept, 1);
+		
+		
+		boolean isAddedtoEmp=Employee.addEmployee(addedUIN);
+		
+		if(isAddedtoEmp)
+			isAdded=true;
+			
+		
+		
+		
+		
+	return isAdded;	
+	}
+	
 
 }
