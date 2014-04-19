9f700b850e5b9befe23245ad6353efd6bce7d131
Simant <simant.purohit@gmail.com>
2014-04-19 02:22:21 -0500

    Removed some locations

--- CS597-PROJECT/src/ClassroomLocation.java
+++ CS597-PROJECT/src/ClassroomLocation.java
@@ -2,15 +2,27 @@
 public enum ClassroomLocation {
 	//List of all locations
 	LOCATION1,
-	LOCATION2,
-	LOCATION3,
-	LOCATION4,
-	LOCATION5,
-	LOCATION6,
-	LOCATION7,
-	LOCATION8,
-	LOCATION9,
-	LOCATION10,
-	LOCATION11,
-	LOCATION12
+	LOCATION2;
+//	LOCATION3,
+//	LOCATION4,
+//	LOCATION5,
+//	LOCATION6
+//	LOCATION7,
+//	LOCATION8,
+//	LOCATION9,
+//	LOCATION10,
+//	LOCATION11,
+//	LOCATION12
+	
+	public static String[] getAllLocations(){
+		ClassroomLocation[] cl = ClassroomLocation.values();
+		String[] locs= new String[cl.length];
+		for(int i=0;i<cl.length;i++){
+			locs[i] = cl[i].toString();
+		}
+		
+		return locs;
+	}
+
 }
+
