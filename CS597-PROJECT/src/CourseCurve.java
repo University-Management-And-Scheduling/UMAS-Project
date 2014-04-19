85ed825a1d988ee7f432e0555e98f24b1334f803
Prasad Nair <pnair3@uic.edu>
2014-04-19 11:51:47 +0530

    Minor additions in CourseCurve class

--- CS597-PROJECT/src/CourseCurve.java
+++ CS597-PROJECT/src/CourseCurve.java
@@ -6,15 +6,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 
 public class CourseCurve {
 
 	int offerID;
-	HashMap<String, Integer> curvingCriteria;
+	List<Integer> curvingCriteria;
 	HashMap<Student,String> courseCurve;
 	
 	@Target({ElementType.LOCAL_VARIABLE})
@@ -26,7 +33,7 @@ public class CourseCurve {
 	 boolean[] isSource () default false; 
 	}
 	
-	public CourseCurve(int offerID, HashMap<String, Integer> curvingCriteria) {
+	public CourseCurve(int offerID, List<Integer> curvingCriteria) {
 		super();
 		this.offerID = offerID;
 		this.curvingCriteria = curvingCriteria;
@@ -40,11 +47,11 @@ public class CourseCurve {
 		this.offerID = offerID;
 	}
 
-	public HashMap<String, Integer> getCurvingCriteria() {
+	public List<Integer> getCurvingCriteria() {
 		return curvingCriteria;
 	}
 
-	public void setCurvingCriteria(HashMap<String, Integer> curvingCriteria) {
+	public void setCurvingCriteria(List<Integer> curvingCriteria) {
 		this.curvingCriteria = curvingCriteria;
 	}
 
@@ -120,13 +127,12 @@ public class CourseCurve {
 		
 	}
 	
-	
 	// 3 ways to Calculate the Curve
 
 	// HashMap<String,Integer> = HashMap<Grade,Percentage of students in the grade>
 	// Example: <'A',30> = Top 30% students would be given 'A' grade
 	// <'B',40> = Next 40% students would be given 'B' grade
-	public static CourseCurve calculatePercentageCurve(int offerID, HashMap<String,Integer> curvingCriteria){
+	public static CourseCurve calculatePercentageCurve(int offerID, List<Integer> curvingCriteria){
 		CourseCurve curve = new CourseCurve(offerID,curvingCriteria);
 		
 		// STEP 1: Get total CourseMarks from CourseStructure Table
@@ -135,40 +141,70 @@ public class CourseCurve {
 		// STEP 2: get totalMarks for each student from CourseExams' Table
 		HashMap<Student,Double> studentTotalMarks = curve.getStudentTotalMarks();
 		
-		// STEP 3: Calculate curve for the courseOffered based on
+		// STEP 3: Sort the hashmap in descending order
+		HashMap<Student,Double> sortedstudentTotalMarks = sortHashMap(studentTotalMarks);
+		
+		// STEP 4: Calculate curve for the courseOffered based on
+		// the curvingCriteria selected by the professor 
+		//(30,40,30) = (9,12,9) / 30
+		HashMap<Student,String> courseCurve = null;
+		int initialStudents = 0;
+		int thirtypercent = (int) Math.ceil(0.7 * 30);
+		int numberOfStudents = sortedstudentTotalMarks.size();
+		for (int percentStudents: curvingCriteria) {
+			
+			//int percentStudents = curvingCriteria.get(initialStudents);
+			int students =  (int) Math.ceil((percentStudents/100) * numberOfStudents);
+			
+			for(int studentsAdded = 0; studentsAdded<=students;studentsAdded++){
+				// Skip
+				int UIN = 0;
+				double marks = 0.0;
+				Set<Student> keys = sortedstudentTotalMarks.keySet();
+				Iterator<Student> keyIterator = keys.iterator();
+				for(int i = 0;i<=initialStudents;i++){
+					if (keyIterator.hasNext()) {
+						Student student = keyIterator.next();
+					}
+				}
+				Student student = keyIterator.next();
+				
+				initialStudents++;
+				
+			}
+			
+		}
+		curve.setCourseCurve(courseCurve);
+		return curve;
+	}
+
+	// HashMap<String,Integer> = HashMap<Grade,CutofPercentage>
+	// Example: <'A',90> = Students with total mks at or above 90-100% would be given 'A' grade
+	// <'B',75> = Students with total mks between 75-89% would be given 'B' grade
+	public static CourseCurve calculateAbsoluteCurve(int offerID, List<Integer> curvingCriteria){
+		CourseCurve curve = new CourseCurve(offerID,curvingCriteria);
+		
+		// STEP 1: Get total CourseMarks from CourseStructure Table
+		int totalCourseMarks = curve.getTotalCourseMarks();
+		
+		// STEP 2: get totalMarks for each student from CourseExams' Table
+		HashMap<Student,Double> studentTotalMarks = curve.getStudentTotalMarks();
+		
+		// STEP 3: Sort the hashmap in descending order
+		HashMap<Student,Double> sortedstudentTotalMarks = sortHashMap(studentTotalMarks);
+		
+		// STEP 4: Calculate curve for the courseOffered based on
 		// the curvingCriteria selected by the professor 
 		HashMap<Student,String> courseCurve = null;
 		
-		Set<Student> keys = studentTotalMarks.keySet();
+		Set<Student> keys = sortedstudentTotalMarks.keySet();
 		Iterator<Student> keyIterator = keys.iterator();
 		while (keyIterator.hasNext()) {
 			Student student = keyIterator.next();
 			int UIN = student.getUIN();
-			double marks = (double) studentTotalMarks.get(student);
+			double marks = (double) sortedstudentTotalMarks.get(student);
 			
-		}	
-		
-		curve.setCourseCurve(courseCurve);
-		return curve;
-	}
-	
-	// HashMap<String,Integer> = HashMap<Grade,CutofPercentage>
-	// Example: <'A',90> = Students with total mks at or above 90-100% would be given 'A' grade
-	// <'B',75> = Students with total mks between 75-89% would be given 'B' grade
-	public static CourseCurve calculateAbsoluteCurve(int offerID, HashMap<String,Integer> curvingCriteria){
-		CourseCurve curve = new CourseCurve(offerID,curvingCriteria);
-		
-		// STEP 1: Get total CourseMarks from CourseStructure Table
-		int totalCourseMarks = curve.getTotalCourseMarks();
-		
-		// STEP 2: get totalMarks for each student from CourseExams' Table
-		HashMap<Student,Double> studentTotalMarks = curve.getStudentTotalMarks();
-		
-		// STEP 3: Calculate curve for the courseOffered based on
-		// the curvingCriteria selected by the professor 
-		HashMap<Student,String> courseCurve = null;
-		
-		// Algo to calculate curve
+		}
 		
 		curve.setCourseCurve(courseCurve);
 		return curve;
@@ -178,7 +214,7 @@ public class CourseCurve {
 	// HashMap<String,Integer> = HashMap<Grade,Minimum Number of students in that grade>
 	// Example: <'A',10> = Atleast top 10 students get 'A' grade 
 	// <'B',10> = After all 'A' grades, atleast 10 students will be given 'B' grade
-	public static CourseCurve calculateMaxGapCurve(int offerID, HashMap<String,Integer> curvingCriteria){
+	public static CourseCurve calculateMaxGapCurve(int offerID, List<Integer> curvingCriteria){
 		CourseCurve curve = new CourseCurve(offerID,curvingCriteria);
 		
 		// STEP 1: Get total CourseMarks from CourseStructure Table
@@ -187,7 +223,10 @@ public class CourseCurve {
 		// STEP 2: get totalMarks for each student from CourseExams' Table
 		HashMap<Student,Double> studentTotalMarks = curve.getStudentTotalMarks();
 		
-		// STEP 3: Calculate curve for the courseOffered based on
+		// STEP 3: Sort the hashmap in descending order
+		HashMap<Student,Double> sortedstudentTotalMarks = sortHashMap(studentTotalMarks);
+		
+		// STEP 4: Calculate curve for the courseOffered based on
 		// the curvingCriteria selected by the professor 
 		HashMap<Student,String> courseCurve = null;
 		
@@ -203,5 +242,25 @@ public class CourseCurve {
 		return curve;
 	}
 	
-	
+	private static HashMap<Student, Double> sortHashMap(HashMap<Student, Double> unsortedStudentTotalMarks)
+    {
+
+        List<Entry<Student, Double>> list = new LinkedList<Entry<Student, Double>>(unsortedStudentTotalMarks.entrySet());
+
+        // Sorting the list based on values
+        Collections.sort(list, new Comparator<Entry<Student, Double>>()
+        {
+            public int compare(Entry<Student, Double> studentTotalMarks1, Entry<Student, Double> studentTotalMarks2) {
+                return studentTotalMarks2.getValue().compareTo(studentTotalMarks1.getValue());
+            }
+        });
+
+        // Maintaining insertion order with the help of LinkedList
+        HashMap<Student, Double> sortedstudentTotalMarks = new LinkedHashMap<Student, Double>();
+        for (Entry<Student, Double> entry : list) {
+        	sortedstudentTotalMarks.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedstudentTotalMarks;
+    }
 }
