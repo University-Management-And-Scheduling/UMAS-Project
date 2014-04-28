21f979ad0bfbe5637a12c2bfdd67a5a52ee5ff07
Simant <simant.purohit@gmail.com>
2014-04-28 01:41:20 -0500

    Minor changes

--- CS597-PROJECT/src/ClassroomLocation.java
+++ CS597-PROJECT/src/ClassroomLocation.java
@@ -6,15 +6,7 @@ public enum ClassroomLocation {
 	LOCATION1,
 	LOCATION2,
 	LOCATION3;
-//	LOCATION4,
-//	LOCATION5,
-//	LOCATION6
-//	LOCATION7,
-//	LOCATION8,
-//	LOCATION9,
-//	LOCATION10,
-//	LOCATION11,
-//	LOCATION12
+	
 	
 	public static String[] getAllLocations(){
 		ClassroomLocation[] cl = ClassroomLocation.values();
