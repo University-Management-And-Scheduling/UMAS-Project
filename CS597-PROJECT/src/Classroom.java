4f768f8fad734a5c4be8c78f182b2ed79ef96c39
Simant <simant.purohit@gmail.com>
2014-05-01 05:39:53 -0500

    Minor change

--- CS597-PROJECT/src/Classroom.java
+++ CS597-PROJECT/src/Classroom.java
@@ -1,10 +1,6 @@
 
 /****************@author Simant Purohit*********************************/
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
